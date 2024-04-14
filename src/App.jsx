1c00194583697ff426e2ac00e57c89fdd4b95142
fasfashann <saintgloria.jkt@gmail.com>
2024-04-14 22:31:57 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-
-import "./App.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [toParam, setToParam] = useState("teman-teman");
+
   useEffect(() => {
     // Mendapatkan nilai 'namaundangan' dari parameter URL
     const urlParams = new URLSearchParams(window.location.search);
@@ -18,9 +16,44 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl">The Wedding of Windy Maldini</h1>
-      <p className="text-black">Teruntuk {toParam}</p>
-      <img width={600} src="/og-image.jpg" alt="" />
+      <img width={1440} src="/hero.jpg" alt="" />
+      <div className="p-5 mt-10 text-center">
+        <h1 className="md:text-7xl text-4xl  text-center font-title ">
+          The Wedding of <br /> Windy & Maldini
+        </h1>
+        <p className="text-center font-title md:text-4xl text-2xl mt-4">
+          02.06.2024
+        </p>
+        <p className="text-center font-body mt-6">
+          “ Dan di antara tanda-tanda kebesaran-Nya ialah diciptakannya untukmu
+          pasangan hidup dari jenismu sendiri supaya kamu cenderung dan merasa
+          tentram kepadanya dan dijadikan-Nya di antaramu rasa kasih dan sayang.
+          Sesungguhnya pada yang demikian itu menjadivtanda-tanda bagi kaum yang
+          berpikir. “ Ar-Rum : 21
+        </p>
+        <h2 className="mt-10 font-title text-2xl">
+          {" "}
+          Bismillahirrahmannirrahiim
+        </h2>
+        <p className="text-center font-body mt-6">
+          Dengan memohon Rahmat & Ridho Allah SWT, kami bermaksud mengundang
+          teman-teman sekalian untuk menghadiri acara pernikahan kami
+        </p>
+        <div className="mt-10 space-y-10">
+          <div className="space-y-2">
+            <h3 className="font-title text-3xl">Windy Anggraini</h3>
+            <p className="font-body">
+              Putri kedua dari Bapak Wahyu Wiguna dan Ibu Mulyanih{" "}
+            </p>
+          </div>
+          <div className="space-y-2">
+            <h3 className="font-title text-3xl">Maldini Ramadhan</h3>
+            <p className="font-body">
+              Putra pertama dari Bapak Kurniadi dan Ibu Rini Kurniawati
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      title: ['"Libre Caslon Display"'],
+      body: ['"Urbanist"'],
+    },
   },
   plugins: [],
 };
